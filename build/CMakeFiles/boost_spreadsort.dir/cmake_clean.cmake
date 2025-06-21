b96f41b6f62285fc72037be0110847bc62486fa4
Amit Joshi <amit.joshiusa@gmail.com>
2025-06-20 22:06:39 -0400

    tryna get spreadsort working

---
+++ build/CMakeFiles/boost_spreadsort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost_spreadsort.dir/spreadsort.cpp.o"
+  "CMakeFiles/boost_spreadsort.dir/spreadsort.cpp.o.d"
+  "boost_spreadsort.cpython-313-darwin.so"
+  "boost_spreadsort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boost_spreadsort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
